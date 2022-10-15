d3cec3985cd2a0807b36b6111c53547460c14258
Fmvmedeiros <84106516+Fmvmedeiros@users.noreply.github.com>
2022-10-15 10:13:31 +0100

    Updates

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
 control.onEvent(EventBusSource.MICROBIT_ID_ACCELEROMETER, EventBusValue.MICROBIT_ACCELEROMETER_EVT_DATA_UPDATE, function () {
     forca = input.acceleration(Dimension.Strength)
-    if (forca > 1700) {
+    if (forca > 1200) {
+        strip.setPixelColor(0, neopixel.hsl(Math.map(Math.constrain(forca, 1600, 2000), 1600, 2000, 250, 0), 80, 10))
+        strip.setPixelColor(0, neopixel.hsl(Math.map(Math.constrain(forca, 1600, 2000), 1600, 2000, 250, 0), 80, 20))
+        strip.setPixelColor(0, neopixel.hsl(Math.map(Math.constrain(forca, 1600, 2000), 1600, 2000, 250, 0), 80, 30))
+        strip.setPixelColor(0, neopixel.hsl(Math.map(Math.constrain(forca, 1600, 2000), 1600, 2000, 250, 0), 80, 40))
         strip.setPixelColor(0, neopixel.hsl(Math.map(Math.constrain(forca, 1600, 2000), 1600, 2000, 250, 0), 80, 50))
     }
 })
 let forca = 0
 let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P0, 27, NeoPixelMode.RGB)
 strip.showColor(neopixel.colors(NeoPixelColors.Orange))
 basic.forever(function () {
     strip.shift(1)
     strip.show()
-    basic.pause(20)
+    basic.pause(5)
 })
